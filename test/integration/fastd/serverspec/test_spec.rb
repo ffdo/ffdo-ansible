b3410e75bf877058edc43f9595c6ee4c5fd648b5
Till Klocke <till.klocke@gmail.com>
2015-07-23 14:47:12 +0200

    Added tests for rng-tools to serverspec based tests

--- test/integration/fastd/serverspec/test_spec.rb
+++ test/integration/fastd/serverspec/test_spec.rb
@@ -6,3 +6,12 @@ set :backend, :exec
 describe package('fastd') do
   it { should be_installed }
 end
+
+describe package('rng-tools') do
+  it { should be_installed }
+end
+
+describe file('/etc/default/rng-tools') do
+  it { should exist }
+  its(:content) { should match /HRNGDEVICE=\/dev\/urandom/ }
+end

d9cbd4aa71785e7985a7894dc1b91ca6dcf3e13c
Till Klocke <till.klocke@gmail.com>
2015-07-25 14:24:07 +0200

    Extended test case

--- test/integration/supernode/serverspec/test_spec.rb
+++ test/integration/supernode/serverspec/test_spec.rb
@@ -119,3 +119,8 @@ end
 describe cron do
   it { should have_entry '*/5 * * * * wget -q -O /etc/fastd/fastd-blacklist.json https://raw.githubusercontent.com/ffruhr/fastdbl/master/fastd-blacklist.json' }
 end
+
+describe user('fritz') do
+  it { should exist }
+  it { should have_authorized_key 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCfO2TgNUMcY9K8N9SsAU/LfVtlloO50n6NRxDER8dCi+t1ydalTbNdEj52cIjqvIEn458cCxdCwsNYeWqSVggX7vT7a+DgYZlJcRteHcb0lPYZplPuiZe3AUE9rHH48W4xEopv7J8Wz6hLng6Gb0TQx9HeVybQ5dn6CGyt8sOCKQ== Fritz Brinkhoffs' }
+end

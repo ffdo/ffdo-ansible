4bcbbe242878daa5a5b1ff81dde59d9b1bb8616d
Till Klocke <till.klocke@gmail.com>
2015-07-27 20:40:46 +0200

    Added some more test examples

--- test/integration/supernode/serverspec/bird_spec.rb
+++ test/integration/supernode/serverspec/bird_spec.rb
@@ -4,21 +4,11 @@ describe package('bird') do
   it { should be_installed }
 end
 
-describe file('/etc/bird/bird.conf') do
-  it { should exist }
-  its(:content) { should contain 'router id 10.42.0.1;' }
-end
-
-describe file('/etc/bird/bird6.conf') do
-  it { should exist }
-  its(:content) { should contain 'router id 10.42.0.1;' }
-end
-
 # At least on Ubuntu doesn't open a port. Is this a problem?
-#describe port(179) do 
-#  it { should be_listening.with('tcp') }
-#  it { should be_listening.with('tcp6') }
-#end
+describe port(179) do 
+  it { should be_listening.with('tcp') }
+  it { should be_listening.with('tcp6') }
+end
 
 #describe service('bird') do
 #  it { should be_running }
--- test/integration/supernode/serverspec/fastd_spec.rb
+++ test/integration/supernode/serverspec/fastd_spec.rb
@@ -13,12 +13,6 @@ describe service('fastd') do
 #  it { should be_enabled }
 end
 
-# Detection of running service fails in Debian 7.8. Needs investigation.
-#describe service('rng-tools') do
-#  it { should be_running }
-#  it { should be_enabled }
-#end
-
 describe file('/etc/fastd/fastd-blacklist.sh') do
   it { should exist }
   it { should be_executable }
@@ -39,6 +33,16 @@ end
 describe file('/etc/default/rng-tools') do
   it { should exist }
   its(:content) { should match /HRNGDEVICE=\/dev\/urandom/ }
+end
+
+# Detection of running service fails in Debian 7.8. Needs investigation.
+#describe service('rng-tools') do
+#  it { should be_running }
+#  it { should be_enabled }
+#end
+
+describe process("rngd") do
+  it { should be_running }
 end
 
 describe user('fastd') do

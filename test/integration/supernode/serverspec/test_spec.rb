6a49e7f7a1421ef454a349763375f46f3a216bda
Till Klocke <till.klocke@gmail.com>
2015-07-30 14:07:06 +0200

    We are now adding the test user to a group and checking this with serverspec

--- test/integration/supernode/serverspec/test_spec.rb
+++ test/integration/supernode/serverspec/test_spec.rb
@@ -7,6 +7,7 @@ end
 describe user('fritz') do
   it { should exist }
   it { should have_authorized_key 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCfO2TgNUMcY9K8N9SsAU/LfVtlloO50n6NRxDER8dCi+t1ydalTbNdEj52cIjqvIEn458cCxdCwsNYeWqSVggX7vT7a+DgYZlJcRteHcb0lPYZplPuiZe3AUE9rHH48W4xEopv7J8Wz6hLng6Gb0TQx9HeVybQ5dn6CGyt8sOCKQ== Fritz Brinkhoffs' }
+  it { should belong_to_group 'sudo' }
 end
 
 describe port(67) do

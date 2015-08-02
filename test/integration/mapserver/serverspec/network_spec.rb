2ff92f494ae56f2cb4c767b3cdf486233592a5a3
Till Klocke <till.klocke@gmail.com>
2015-08-02 23:11:18 +0200

    Lots of changes to get a working mapserver

--- test/integration/mapserver/serverspec/network_spec.rb
+++ test/integration/mapserver/serverspec/network_spec.rb
@@ -15,3 +15,21 @@ end
 describe interface('eth0') do
   it { should exist }
 end
+
+describe package('bird') do
+  it { should be_installed }
+end
+
+# At least on Ubuntu doesn't open a port. Is this a problem?
+describe port(179) do 
+  it { should be_listening.with('tcp') }
+  it { should be_listening.with('tcp6') }
+end
+
+describe process('bird') do
+  it { should be_running }
+end
+
+describe process('bird6') do
+  it { should be_running }
+end
--- test/integration/mapserver/serverspec/test_spec.rb
+++ test/integration/mapserver/serverspec/test_spec.rb
@@ -7,3 +7,15 @@ describe file('/usr/local/bin/alfred-json') do
   it { should exist }
   it { should be_executable }
 end
+
+describe cron do {
+  it { should should have_entry '* * * * * /srv/ffmap-backend/backend.py -d /srv/ffmap-data -p 14' }
+}
+
+describe port(80) do
+  it { should be_listening.with('tcp6') }
+end
+
+describe port(443) do
+  it { should be_listeing.with('tcp6') }
+end

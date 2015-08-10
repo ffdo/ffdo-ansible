b1abb888a87e3cc597780d78cc16c4c31ddc2dbc
Till Klocke <till.klocke@gmail.com>
2015-08-10 21:33:22 +0200

    Removed cron test example since we are now installing a file to cron.d

--- test/integration/mapserver/serverspec/test_spec.rb
+++ test/integration/mapserver/serverspec/test_spec.rb
@@ -8,10 +8,6 @@ describe file('/usr/local/bin/alfred-json') do
   it { should be_executable }
 end
 
-describe cron do 
-  it { should have_entry '* * * * * /opt/ffmap-backend/backend.py -d /srv/ffmap-data -p 14' }
-end
-
 describe port(80) do
   it { should be_listening.with('tcp6') }
 end

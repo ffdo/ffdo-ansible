c02a1bf88c7ff588e022ea7274582e8f5a12e199
Markus Lindenberg <lindenberg@gonicus.de>
2016-02-28 10:47:49 +0100

    (temporarily) remove cruft

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source "https://rubygems.org"
-
-gem "test-kitchen"
-gem "kitchen-ansiblepush"
-gem "kitchen-ansible"
-gem "kitchen-vagrant"
--- test/integration/mapserver/serverspec/batman_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'serverspec'
-
-describe package('batctl') do
-  it {should be_installed }
-end
-
-describe kernel_module('batman_adv') do
-  it { should be_loaded }
-end
